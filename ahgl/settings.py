1be87e6dade4a24a1ae3bdecad8d40d2283342e7
Nathaniel Tucker <me@ntucker.me>
2012-04-13 22:35:15 -0700

    We don't need the sites app

--- ahgl/settings.py
+++ ahgl/settings.py
@@ -53,8 +53,6 @@ LANGUAGES = (
     ('en', gettext('English')),
 )
 
-SITE_ID = 1
-
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
@@ -168,7 +166,6 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
-    "django.contrib.sites",
     "django.contrib.messages",
     "django.contrib.humanize",
     "django.contrib.redirects",
