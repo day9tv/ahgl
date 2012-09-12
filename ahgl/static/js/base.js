e4b523f0493c4d2bf38d96d7d9dd0734ded49182
Nathaniel Tucker <me@ntucker.me>
2012-09-11 19:17:55 -0700

    Unstale the js cache?

--- ahgl/static/js/base.js
+++ ahgl/static/js/base.js
@@ -278,7 +278,7 @@ jQuery.extend(jQuery.easing,
 * 
 * Open source under the BSD License. 
 * 
-* Copyright Â© 2001 Robert Penner
+* Copyright © 2001 Robert Penner
 * All rights reserved.
 * 
 * Redistribution and use in source and binary forms, with or without modification, 
